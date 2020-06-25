339eef881402631c8300cdeffce3b06b1d64c051
WeRockStar <kotchaphan.m@gmail.com>
2020-06-25 14:45:08 +0700

    Add Hilt annotation

---
+++ app/src/main/java/com/werockstar/hilt/HiltApplication.kt
@@ -0,0 +1,14 @@
+package com.werockstar.hilt
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class HiltApplication: Application() {
+
+	override fun onCreate() {
+		super.onCreate()
+
+
+	}
+}
